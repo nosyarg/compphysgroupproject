f6b547393d5ce35e765527f38f8c473470b654a1
nosyarg <york17a@ncssm.edu>
2017-04-25 19:02:02 -0400

    first working code

--- analysis.py
+++ analysis.py
@@ -1,10 +1,10 @@
 from astropy.constants import G
 from scipy.optimize import *
 from math import *
-#planet names
 #source for ellipse parameterization: http://www.pa.msu.edu/~stump/champ/mech3f01.pdf
 #Source for year lengths: https://www.exploratorium.edu/ronh/age/
 #source for eccentricity calculations: https://socratic.org/questions/if-an-asteroid-has-a-perihelion-distance-of-2-0-a-u-and-an-aphelion-distance-of--1
+#planet names
 planets = ["Mercury", "Venus", "Earth", "Moon", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 #mass constants
 mass = [0.33e24, 4.87e24, 5.97e24, 0.073e24, 0.642e24, 1898e24, 568e24, 86.8e24, 102e24]
@@ -24,21 +24,22 @@ for i in range(len(planets)):
         y.append(aphelion[i])
 #orbital period in years:
 period = [87.97,224.7,365.26,1.88*365,11.86*365,29.46*365,84.01*365,164.79*365,248.59*365]
+for i in range(len(period)):
+        period[i] = period[i]*12/365
 def time(psi,pnum):
         return period[pnum]/(2*pi)*(psi-ecc[pnum]*sin(psi))
-timescale = 365 #time of simulation in days
-for t in range(1,timescale):
-        print("Positions at day " + str(t) + ":")
-        x = []
-        y = []
-        for i in range(len(planets)):
-                #set up function for error in time estimate for numerical soln
-                def timeerror(psi):
-                        return abs(t - time(psi,i))
-                #numerically find value of psi for posn. equations
-                psi = minimize(timeerror,pi).x
-                x.append((perihelion[i]+aphelion[i])/2*(cos(psi)-ecc[i]))
-                y.append((perihelion[i]+aphelion[i])/2*sqrt(1-ecc[i]**2)*sin(psi))
-                print(planets[i]+": ("+str(x[i])+","+str(y[i])+")")
+t = int(input("How many months after alignment? (input negative number for past):"))
+print("Positions at day " + str(t) + ":")
+x = []
+y = []
+for i in range(len(planets)):
+        #set up function for error in time estimate for numerical soln
+        def timeerror(psi):
+                return abs(t - time(psi,i))
+        #numerically find value of psi for posn. equations
+        psi = minimize(timeerror,pi).x
+        x.append((perihelion[i]+aphelion[i])/2*(cos(psi)-ecc[i]))
+        y.append((perihelion[i]+aphelion[i])/2*sqrt(1-ecc[i]**2)*sin(psi))
+        print(planets[i]+": ("+str(x[i])+" m,"+str(y[i])+" m) "+str(180/pi*atan2(y[i],x[i]))+" degrees")
 #for i in range(len(planets))
 #v = sqrt(G*M/r)
